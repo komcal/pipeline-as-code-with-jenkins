6e1de5005f9acebfbc215dc46f0cdcd45b9e5d55
komcal <kom.sujautra@gmail.com>
2017-04-19 10:29:33 +0700

    add simple test

---
+++ test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+describe('Test Operation ', function() {
+  it('1 + 1 should return 2', function() {
+    assert.equal(2, 1 + 1);
+  });
+  it('1 - 1 should return 0', function() {
+    assert.equal(0, 1 - 1);
+  });
+});
